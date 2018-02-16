dd189c85da810bff134657cf3e7d3cf13fa2fc2f
homahi <homahi0128@gmail.com>
2018-02-16 09:15:50 +0900

    element-uiを入れた

--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>ここはヘッダーです。</div>
+<el-menu :default-active="activeIndex" mode="horizontal" class="el-menu-demo">
+<el-menu-item index="1">Center</el-menu-item>
+<el-menu-item index="2">Second</el-menu-item>
+</el-menu>
 </template>
+
+<script type="text/javascript">
+export default {
+  data () {
+    return {
+      activeIndex: '1'
+    }
+  }
+}
+</script>
